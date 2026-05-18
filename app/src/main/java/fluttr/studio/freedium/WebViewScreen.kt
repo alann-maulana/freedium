f915f9cbf4a140e16d794d75d91ff163994d9d42
alann-maulana <alann_it06@yahoo.co.id>
2026-05-18 16:02:45 +0700

    feat: add button to share webview url

--- app/src/main/java/fluttr/studio/freedium/WebViewScreen.kt
+++ app/src/main/java/fluttr/studio/freedium/WebViewScreen.kt
@@ -1,6 +1,7 @@
 package fluttr.studio.freedium
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Bitmap
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
@@ -11,11 +12,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -26,6 +29,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WebViewScreen(url: String, onClose: () -> Unit) {
+    val context = LocalContext.current
     var webViewInstance by remember { mutableStateOf<WebView?>(null) }
     var loadProgress by remember { mutableFloatStateOf(0f) }
     var isPageLoading by remember { mutableStateOf(true) }
@@ -57,6 +61,16 @@ fun WebViewScreen(url: String, onClose: () -> Unit) {
                     }
                 },
                 actions = {
+                    IconButton(onClick = {
+                        val shareIntent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, url)
+                            type = "text/plain"
+                        }
+                        context.startActivity(Intent.createChooser(shareIntent, "Share Article Link"))
+                    }) {
+                        Icon(imageVector = Icons.Default.Share, contentDescription = "Share")
+                    }
                     IconButton(onClick = { webViewInstance?.reload() }) {
                         Icon(imageVector = Icons.Default.Refresh, contentDescription = "Refresh")
                     }
