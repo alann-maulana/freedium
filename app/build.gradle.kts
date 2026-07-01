1f6b3447ca8b2c65588f759011a74a02f660d948
alann-maulana <alann_it06@yahoo.co.id>
2026-07-01 12:18:14 +0700

    chore: update version 1.1.0+3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "fluttr.studio.freedium"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
